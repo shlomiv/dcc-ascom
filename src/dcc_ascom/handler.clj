9a33c53ac41ac54e5eb6ac607576219020cc166d
Shlomi <shlomivaknin@gmail.com>
2019-08-05 00:01:06 -0700

    Factor out ascom response

--- src/dcc_ascom/handler.clj
+++ src/dcc_ascom/handler.clj
@@ -13,186 +13,107 @@
 (defn next-transaction[]
   (swap! transaction-id inc))
 
+(defn ascom-response
+  ([client-transaction-id err-code] (ascom-response client-transaction-id err-code nil))
+  ([client-transaction-id err-code response]
+   (cond 
+     (#{200} err-code) {:status 200
+                         :body (let [r {"ClientTransactionID" client-transaction-id
+                                        "ServerTransactionID" (next-transaction)
+                                        "ErrorNumber" 0
+                                        "ErrorMessage" ""}
+                                     resp (if (nil? response) r (assoc r "Value" response) )]
+                                 (println "Responding: "resp)
+                                 resp)}
+     (#{400 500} err-code) {:status 400 :body response}
+     :else {:status 500 :body (str "Dont know err-code (" err-code "): " response)})))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET "/api/v1/camera/:id/interfaceversion" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 2}})
+       (ascom-response ClientTransactionID 200 2))
+
   (GET "/api/v1/camera/:id/connected" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" true}})
+       (ascom-response ClientTransactionID 200 true))
+
   (PUT "/api/v1/camera/:id/connected" [id Connected ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (GET "/api/v1/camera/:id/sensorname" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" ""}})
+       (ascom-response ClientTransactionID 200 ""))
+
   (GET "/api/v1/camera/:id/canasymmetricbin" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" false}})
+       (ascom-response ClientTransactionID 200 false))
+
   (GET "/api/v1/camera/:id/binx" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 1}})
+       (ascom-response ClientTransactionID 200 1))
+
   (GET "/api/v1/camera/:id/biny" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 1}})
+       (ascom-response ClientTransactionID 200 1))
+
   (PUT "/api/v1/camera/:id/binx" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (PUT "/api/v1/camera/:id/biny" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (GET "/api/v1/camera/:id/maxbinx" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 1}})
+       (ascom-response ClientTransactionID 200 1))
+
   (GET "/api/v1/camera/:id/maxbiny" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 1}})
+       (ascom-response ClientTransactionID 200 1))
+
   (GET "/api/v1/camera/:id/cameraxsize" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 6004}})
+       (ascom-response ClientTransactionID 200 6004))
+
   (GET "/api/v1/camera/:id/cameraysize" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 4004}})
+       (ascom-response ClientTransactionID 200 4004))
+
   (GET "/api/v1/camera/:id/sensortype" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 0}})
+       (ascom-response ClientTransactionID 200 0))
+
   (GET "/api/v1/camera/:id/maxadu" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 65535}})
+       (ascom-response ClientTransactionID 200 65535))
+
   (GET "/api/v1/camera/:id/startx" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 0}})
+       (ascom-response ClientTransactionID 200 0))
+
   (GET "/api/v1/camera/:id/starty" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 0}})
+       (ascom-response ClientTransactionID 200 0))
+
   (PUT "/api/v1/camera/:id/startx" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (PUT "/api/v1/camera/:id/starty" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (GET "/api/v1/camera/:id/gainmin" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 100}})
+       (ascom-response ClientTransactionID 200 100))
+
   (GET "/api/v1/camera/:id/gainmax" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 6400}})
+       (ascom-response ClientTransactionID 200 6400))
+
+  (GET "/api/v1/camera/:id/gain" [id ClientID ClientTransactionID]
+       (ascom-response ClientTransactionID 200 3)) ;; index into gains array
+
+  (PUT "/api/v1/camera/:id/gain" [id Gain ClientID ClientTransactionID]
+       (ascom-response ClientTransactionID 200 3 Gain)) ;; index into gains array
+
   (GET "/api/v1/camera/:id/driverinfo" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" "DCC - Clojure ASCOM bridge"}})
+       (ascom-response ClientTransactionID 200 "DCC - Clojure ASCOM bridge"))
+
   (GET "/api/v1/camera/:id/exposuremin" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 1}})
+       (ascom-response ClientTransactionID 200 1))
+
   (GET "/api/v1/camera/:id/exposuremax" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 3600}})
+       (ascom-response ClientTransactionID 200 3600))
+
   (GET "/api/v1/camera/:id/cooleron" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" false}})
-  
+       (ascom-response ClientTransactionID 200 false))
+
   (GET "/api/v1/camera/:id/cansetccdtemperature" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" false}})
+       (ascom-response ClientTransactionID 200 false))
+
   (GET "/api/v1/camera/:id/ccdtemperature" [id ClientID ClientTransactionID]
        {:status 500 :body "Not supported (Thanks Nikon!)"})
   (GET "/api/v1/camera/:id/setccdtemperature" [id ClientID ClientTransactionID]
@@ -200,60 +121,29 @@
   (GET "/api/v1/camera/:id/heatsinktemperature" [id ClientID ClientTransactionID]
        {:status 500 :body "Not supported (Thanks Nikon!)"})
   (GET "/api/v1/camera/:id/cangetcoolerpower" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" false}})
+       (ascom-response ClientTransactionID 200 false))
+
   (GET "/api/v1/camera/:id/numx" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 6004}})
+       (ascom-response ClientTransactionID 200 6004))
+
   (GET "/api/v1/camera/:id/numy" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" 4004}})
+       (ascom-response ClientTransactionID 200 4004))
+
   (PUT "/api/v1/camera/:id/numx" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (PUT "/api/v1/camera/:id/numy" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (GET "/api/v1/camera/:id/canabortexposure" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" true}})
+       (ascom-response ClientTransactionID 200 true))
+
   (GET "/api/v1/camera/:id/imageready" [id ClientID ClientTransactionID]
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" false}}) ;;;; TODO:
+       (ascom-response ClientTransactionID 200 false));;;; TODO:
+  
   (GET "/api/v1/camera/:id/camerastate" [id ClientID ClientTransactionID]
        (println "CAMERA STATE " (dcc/get-camera-state @d))
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""
-               "Value" (get dcc/camera-states (dcc/get-camera-state @d))}}) ;; TODO: Complete 
+       (ascom-response ClientTransactionID 200 (get dcc/camera-states (dcc/get-camera-state @d))));; TODO: Complete 
   
   (PUT "/api/v1/camera/:id/startexposure" [id Duration Light ClientID ClientTransactionID]
        (try
@@ -261,39 +151,32 @@
          (try
            (let [_ (swap! d dcc/start-bulb)
                  _ (future
-                     (println "SLEEPING")
                      (Thread/sleep (* 1000 (Integer/parseInt Duration)))
-                     (println "DONE SLEEPING")
                      (swap! d dcc/stop-bulb))]
-             {:status 200
-              :body {"ClientTransactionID" ClientTransactionID
-                     "ServerTransactionID" (next-transaction)
-                     "ErrorNumber" 0
-                     "ErrorMessage" ""}})
+             (ascom-response ClientTransactionID 200))
+
            (catch Exception e
-             {:status 500
-              :body (str e)}))
+             {:status 500 :body (str e)}))
+
          (catch Exception e
-           {:status 400
-            :body "Duration parameter not numeric"}))
-       )
+           {:status 400 :body "Duration parameter not numeric"})))
+  
   (PUT "/api/v1/camera/:id/abortexposure" [id ClientID ClientTransactionID]
        (swap! d dcc/stop-bulb)
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (PUT "/api/v1/camera/:id/stopexposure" [id ClientID ClientTransactionID]
        (swap! d dcc/stop-bulb)
-       {:status 200
-        :body {"ClientTransactionID" ClientTransactionID
-               "ServerTransactionID" (next-transaction)
-               "ErrorNumber" 0
-               "ErrorMessage" ""}})
+       (ascom-response ClientTransactionID 200))
+
   (route/not-found "Not Found"))
 
+
 (def app
-  (wrap-defaults (wrap-json-response (logger/wrap-log-response app-routes))
-                 (assoc-in site-defaults [:security :anti-forgery] false)
-                 ))
+  (-> app-routes
+      logger/wrap-log-response
+      wrap-json-response
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))
+
+
+
